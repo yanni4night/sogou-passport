0b08c4f93a0c0a25760537347818377f4d81155d
yinyong <yinyong@sogou-inc.com>
2014-06-04 16:39:26 +0800

    disabled remindActive,upgrade to 0.1.3

--- src/js/sogou.js
+++ src/js/sogou.js
@@ -15,9 +15,10 @@
  * 2014-05-24[20:43:42]:authorized
  * 2014-05-25[10:48:30]:code matched
  * 2014-06-04[15:16:38]:remove 'container' parameter,we create it instead
- *
+ * 2014-06-04[16:37:06]:disabled remindActive action
+ * 
  * @author yanni4night@gmail.com
- * @version 0.1.2
+ * @version 0.1.3
  * @since 0.1.0
  */
 var UTILS = require('./utils');
@@ -28,6 +29,13 @@ var CODES = require('./codes');
     var FILE_NAME = 'sogou.js';
     var EXPANDO = "sogou-passport-" + (+new Date());
     var HIDDEN_CSS = 'width：1px;height:1px;position:absolute;left:-100000px;';
+
+    var FIXED_URLS = {
+        login: 'https://account.sogou.com/web/login',
+        logout: 'https://account.sogou.com/web/logout_js',
+        //active: 'https://account.sogou.com/web/remindActivate',
+        captcha: 'https://account.sogou.com/captcha'
+    };
 
     var noop = function() {};
     var strundefined = typeof undefined;
@@ -53,7 +61,7 @@ var CODES = require('./codes');
         };
     }
 
-    var _passhtml = '<form method="post" action="https://account.sogou.com/web/login" target="' + EXPANDO + '">' + '<input type="hidden" name="username" value="<%=username%>">' + '<input type="hidden" name="password" value="<%=password%>">' + '<input type="hidden" name="captcha" value="<%=vcode%>">' + '<input type="hidden" name="autoLogin" value="<%=autoLogin%>">' + '<input type="hidden" name="client_id" value="<%=appid%>">' + '<input type="hidden" name="xd" value="<%=redirectUrl%>">' + '<input type="hidden" name="token" value="<%=token%>"></form>' + '<iframe name="' + EXPANDO + '" src="about:blank" style="' + HIDDEN_CSS + '"></iframe>';
+    var _passhtml = '<form method="post" action="' + FIXED_URLS.login + '" target="' + EXPANDO + '">' + '<input type="hidden" name="username" value="<%=username%>">' + '<input type="hidden" name="password" value="<%=password%>">' + '<input type="hidden" name="captcha" value="<%=vcode%>">' + '<input type="hidden" name="autoLogin" value="<%=autoLogin%>">' + '<input type="hidden" name="client_id" value="<%=appid%>">' + '<input type="hidden" name="xd" value="<%=redirectUrl%>">' + '<input type="hidden" name="token" value="<%=token%>"></form>' + '<iframe name="' + EXPANDO + '" src="about:blank" style="' + HIDDEN_CSS + '"></iframe>';
 
     var defaultOptions = {
         appid: null,
@@ -108,7 +116,7 @@ var CODES = require('./codes');
         opt = this.opt = defaultOptions;
 
         if (!options || strobject !== typeof options) {
-            throw new Error('"options" MUST be set as a plain object');
+            throw new Error('"options" MUST be a plain object');
         }
 
         UTILS.mixin(opt, options);
@@ -145,8 +153,7 @@ var CODES = require('./codes');
                 vcode = '';
             }
 
-            //FIXME
-            this._currentUname = username;
+            //this._currentUname = username;
 
             payload = {
                 username: username,
@@ -171,7 +178,7 @@ var CODES = require('./codes');
          */
         logout: function() {
             var self = this;
-            var url = 'https://account.sogou.com/web/logout_js?client_id=' + self.opt.appid;
+            var url = FIXED_URLS.logout + '?client_id=' + self.opt.appid;
             self._assertContainer();
             UTILS.addIframe(this.mHTMLContainer, url, function() {
                 self.opt.onLogoutSuccess();
@@ -188,10 +195,11 @@ var CODES = require('./codes');
                 this.opt.onLoginFailed(data);
             } else if (0 === +data.status) {
                 this.opt.onLoginSuccess(data);
-            } else if (+data.status === 20231) {
-                location.href = 'https://account.sogou.com/web/remindActivate?email=' + encodeURIComponent(this._currentUname) + '&client_id=' + this.opt.appid + '&ru=' + encodeURIComponent(location.href);
-            } else if (+data.needcaptcha) {
-                data.captchaimg = 'https://account.sogou.com/captcha?token=' + this.opt._token + '&t=' + (+new Date());
+            }/* else if (+data.status === 20231) {
+                location.href = FIXED_URLS.active + '?email=' + encodeURIComponent(this._currentUname) + '&client_id=' + this.opt.appid + '&ru=' + encodeURIComponent(location.href);
+            }*/ 
+            else if (+data.needcaptcha) {
+                data.captchaimg = FIXED_URLS.captcha + '?token=' + this.opt._token + '&t=' + (+new Date());
                 this.opt.onLoginFailed(data);
             } else {
                 for (e in CODES) {
@@ -213,7 +221,7 @@ var CODES = require('./codes');
             if (!container || (strobject !== typeof container) || (strundefined === typeof container.appendChild) || !container.parentNode) {
                 container = this.mHTMLContainer = document.createElement('div');
                 container.style.cssText = HIDDEN_CSS;
-                container.className = EXPANDO;
+                container.className = container.id = EXPANDO;
                 document.body.appendChild(container);
             }
 
@@ -410,7 +418,7 @@ var CODES = require('./codes');
 
     //Expose few interfaces
     var PassportSC = {
-        version: '@version@',//from 'package.json'
+        version: '@version@', //from 'package.json'
         /**
          * Initialize.
          * This must be called at first before
