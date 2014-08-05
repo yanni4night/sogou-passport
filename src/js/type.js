d5cae2cc63fa716650ce63bafb1462d869548232
yinyong <yinyong@sogou-inc.com>
2014-08-05 18:05:41 +0800

    remove wrapper of type

--- src/js/type.js
+++ src/js/type.js
@@ -11,110 +11,108 @@
  * @version 0.1.1
  * @since 0.1.0
  */
-(function(undefined) {
-    "use strict";
+"use strict";
 
-    var noop = function() {};
+var noop = function() {};
 
-    var type = {
-        expando: "sogou-passport-" + (+new Date()),
-        noop: noop,
-        debug:+'@debug@',//This has to be replaced with 1 or 0 to indicate a boolean value
-        strundefined: typeof undefined,
-        strstr: typeof '',
-        strobject: typeof {},
-        strnumber: typeof 0,
-        strfunction: typeof noop,
-        isNullOrUndefined: function(obj) {
-            return !!(this.isNull(obj) || this.isUndefined(obj));
-        },
-        isNonNullOrUndefined: function(obj) {
-            return !this.isNullOrUndefined(obj);
-        },
-        isInteger: function(num) {
-            return !!(this.isNumber(num) && /^(\-|\+)?\d+?$/i.test(num));
-        },
-        isNull: function(obj) {
-            return null === obj;
-        },
-        isUndefined: function(obj) {
-            return undefined === obj;
-        },
-        /**
-         * Check if obj is a non-null and non-array object.
-         *
-         * @param  {Object}  obj
-         * @return {Boolean}
-         */
-        isPlainObject: function(obj) {
-            return this.isObject(obj) && !this.isNull(obj) && !this.isArray(obj) && !this.isRegExp(obj) && !this.isDate(obj);
-        },
-        isNonEmptyString: function(obj) {
-            return !!(obj && this.isString(obj));
-        },
-        isHTMLElement: function(obj) {
-            return !!(obj && obj.childNodes && obj.tagName && obj.appendChild);
-        },
-        /**
-         * Check if obj is null,undefined,empty array or empty string.
-         *
-         * @param  {Object}  obj
-         * @return {Boolean}
-         */
-        isEmpty: function(obj) {
-            return this.isNullOrUndefined(obj) || (this.isArray(obj) && !obj.length) || '' === obj;
-        },
-        isObject: function(obj) {
-            return type.strobject === typeof obj;
-        }
-    };
-
-    var typeKeys = "RegExp,Date,String,Array,Boolean,Function,Number".split(',');
-
+var type = {
+    expando: "sogou-passport-" + (+new Date()),
+    noop: noop,
+    debug: +'@debug@', //This has to be replaced with 1 or 0 to indicate a boolean value
+    strundefined: typeof undefined,
+    strstr: typeof '',
+    strobject: typeof {},
+    strnumber: typeof 0,
+    strfunction: typeof noop,
+    isNullOrUndefined: function(obj) {
+        return !!(this.isNull(obj) || this.isUndefined(obj));
+    },
+    isNonNullOrUndefined: function(obj) {
+        return !this.isNullOrUndefined(obj);
+    },
+    isInteger: function(num) {
+        return !!(this.isNumber(num) && /^(\-|\+)?\d+?$/i.test(num));
+    },
+    isNull: function(obj) {
+        return null === obj;
+    },
+    isUndefined: function(obj) {
+        return undefined === obj;
+    },
     /**
-     * Create is* functions.
-     * @param  {String} vari
-     * @return {Function}
+     * Check if obj is a non-null and non-array object.
+     *
+     * @param  {Object}  obj
+     * @return {Boolean}
      */
-    function createIs(vari) {
-        return (function(vari) {
-            return function(variable) {
-                return '[object ' + vari + ']' === ({}).toString.apply(variable);
-            };
-        })(vari);
-    }
-
+    isPlainObject: function(obj) {
+        return this.isObject(obj) && !this.isNull(obj) && !this.isArray(obj) && !this.isRegExp(obj) && !this.isDate(obj);
+    },
+    isNonEmptyString: function(obj) {
+        return !!(obj && this.isString(obj));
+    },
+    isHTMLElement: function(obj) {
+        return !!(obj && obj.childNodes && obj.tagName && obj.appendChild);
+    },
     /**
-     * Create assert function.
-     * @param  {String} vari
-     * @return {Function}
+     * Check if obj is null,undefined,empty array or empty string.
+     *
+     * @param  {Object}  obj
+     * @return {Boolean}
      */
-    function createAssert(vari) {
-        return (function(vari) {
-            return function(name, variable) {
-
-                if (arguments.length < 2) {
-                    variable = name;
-                    name = String(variable);
-                }
-
-                if (!type['is' + vari](variable)) {
-                    throw new Error('"' + name + '" has to be a(n) ' + vari);
-                }
-            };
-        })(vari);
+    isEmpty: function(obj) {
+        return this.isNullOrUndefined(obj) || (this.isArray(obj) && !obj.length) || '' === obj;
+    },
+    isObject: function(obj) {
+        return type.strobject === typeof obj;
     }
+};
 
-    for (var i = typeKeys.length - 1; i >= 0; --i) {
-        type['is' + typeKeys[i]] = createIs(typeKeys[i]);
-        type['assert' + typeKeys[i]] = createAssert(typeKeys[i]);
-    }
+var typeKeys = "RegExp,Date,String,Array,Boolean,Function,Number".split(',');
 
-    //create missing asserts
-    var assertKeys = "Empty,HTMLElement,PlainObject,Undefined,Null,Integer,NullOrUndefined,NonNullOrUndefined,NonEmptyString,Object".split(',');
-    for (i = assertKeys.length; i >= 0; --i) {
-        type['assert' + assertKeys[i]] = createAssert(assertKeys[i]);
-    }
+/**
+ * Create is* functions.
+ * @param  {String} vari
+ * @return {Function}
+ */
+function createIs(vari) {
+    return (function(vari) {
+        return function(variable) {
+            return '[object ' + vari + ']' === ({}).toString.apply(variable);
+        };
+    })(vari);
+}
 
-    module.exports = type;
-})();
+/**
+ * Create assert function.
+ * @param  {String} vari
+ * @return {Function}
+ */
+function createAssert(vari) {
+    return (function(vari) {
+        return function(name, variable) {
+
+            if (arguments.length < 2) {
+                variable = name;
+                name = String(variable);
+            }
+
+            if (!type['is' + vari](variable)) {
+                throw new Error('"' + name + '" has to be a(n) ' + vari);
+            }
+        };
+    })(vari);
+}
+
+for (var i = typeKeys.length - 1; i >= 0; --i) {
+    type['is' + typeKeys[i]] = createIs(typeKeys[i]);
+    type['assert' + typeKeys[i]] = createAssert(typeKeys[i]);
+}
+
+//create missing asserts
+var assertKeys = "Empty,HTMLElement,PlainObject,Undefined,Null,Integer,NullOrUndefined,NonNullOrUndefined,NonEmptyString,Object".split(',');
+for (i = assertKeys.length; i >= 0; --i) {
+    type['assert' + assertKeys[i]] = createAssert(assertKeys[i]);
+}
+
+module.exports = type;
