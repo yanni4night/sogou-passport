6e6e69da7f4a6b0e320768879879555f9075f9af
yinyong <yinyong@sogou-inc.com>
2014-08-05 18:07:10 +0800

    remove wrapper of event

--- src/js/event.js
+++ src/js/event.js
@@ -12,108 +12,106 @@
  * @since 0.1.0
  */
 
-(function(undefined) {
-    "use strict";
+"use strict";
 
-    var console = require('./console');
-    var array = require('./array');
-    var type = require('./type');
-    var string = require('./string');
+var console = require('./console');
+var array = require('./array');
+var type = require('./type');
+var string = require('./string');
 
-    var EventEmitter = function() {
+var EventEmitter = function() {
 
-        var listeners = {};
+    var listeners = {};
 
-        /**
-         * Bind event,multiple events split by space supported
-         * @param  {String} event
-         * @param  {Function} func
-         * @param  {Object} thisArg
-         * @return {EventEmitter}      This event emitter
-         */
-        this.on = function(event, func, thisArg) {
-            var evtArr;
+    /**
+     * Bind event,multiple events split by space supported
+     * @param  {String} event
+     * @param  {Function} func
+     * @param  {Object} thisArg
+     * @return {EventEmitter}      This event emitter
+     */
+    this.on = function(event, func, thisArg) {
+        var evtArr;
 
-            type.assertNonEmptyString('event', event);
-            type.assertFunction('func', func);
+        type.assertNonEmptyString('event', event);
+        type.assertFunction('func', func);
 
-            evtArr = string.trim(event).split(/\s/);
+        evtArr = string.trim(event).split(/\s/);
 
-            array.forEach(evtArr, function(evt) {
-                listeners[evt] = listeners[evt] || [];
-                listeners[evt].push({
-                    type: evt,
-                    func: func,
-                    thisArg: thisArg
-                });
+        array.forEach(evtArr, function(evt) {
+            listeners[evt] = listeners[evt] || [];
+            listeners[evt].push({
+                type: evt,
+                func: func,
+                thisArg: thisArg
             });
+        });
 
-            return this;
-        };
-
-        /**
-         * Remove event,multiple events split by space supported.
-         *
-         * Empty 'func' means remove all listeners named 'event'.
-         *
-         * @param  {String} event
-         * @param  {Function} func
-         * @return {EventEmitter}     This event emitter
-         */
-        this.off = function(event, func) {
-            var evtArr, objs;
-
-            type.assertNonEmptyString('event', event);
-            type.assertFunction('func', func);
-
-            evtArr = string.trim(event).split(/\s/);
-            array.forEach(evtArr, function(evt) {
-                if (!func) {
-                    delete listeners[evt];
-                    return this;
-                } else {
-                    objs = listeners[evt];
-                    if (type.isArray(objs)) {
-                        listeners[evt] = array.filter(objs, function(obj) {
-                            return obj.func !== func;
-                        });
-                    }
-                }
-            });
-
-
-            return this;
-        };
-
-        /**
-         * Emit event(s),multiple events split by space supported.
-         *
-         * @param  {String} event
-         * @param  {Object} data
-         * @return {EventEmitter} This event emitter
-         */
-        this.emit = function(event, data) {
-            var evtArr, objs;
-
-            type.assertNonEmptyString('event', event);
-
-            evtArr = string.trim(event).split(/\s/);
-
-            array.forEach(evtArr, function(evt) {
-                objs = listeners[evt];
-                if (type.isArray(objs)) {
-                    array.forEach(objs, function(obj) {
-                        //add timestamp
-                        obj.timestamp = +new Date();
-                        obj.func.call(obj.thisArg || null, obj, data);
-                    });
-                }
-            });
-
-            console.trace('emitting ' + evtArr.join());
-            return this;
-        };
+        return this;
     };
 
-    module.exports = EventEmitter;
-})();
+    /**
+     * Remove event,multiple events split by space supported.
+     *
+     * Empty 'func' means remove all listeners named 'event'.
+     *
+     * @param  {String} event
+     * @param  {Function} func
+     * @return {EventEmitter}     This event emitter
+     */
+    this.off = function(event, func) {
+        var evtArr, objs;
+
+        type.assertNonEmptyString('event', event);
+        type.assertFunction('func', func);
+
+        evtArr = string.trim(event).split(/\s/);
+        array.forEach(evtArr, function(evt) {
+            if (!func) {
+                delete listeners[evt];
+                return this;
+            } else {
+                objs = listeners[evt];
+                if (type.isArray(objs)) {
+                    listeners[evt] = array.filter(objs, function(obj) {
+                        return obj.func !== func;
+                    });
+                }
+            }
+        });
+
+
+        return this;
+    };
+
+    /**
+     * Emit event(s),multiple events split by space supported.
+     *
+     * @param  {String} event
+     * @param  {Object} data
+     * @return {EventEmitter} This event emitter
+     */
+    this.emit = function(event, data) {
+        var evtArr, objs;
+
+        type.assertNonEmptyString('event', event);
+
+        evtArr = string.trim(event).split(/\s/);
+
+        array.forEach(evtArr, function(evt) {
+            objs = listeners[evt];
+            if (type.isArray(objs)) {
+                array.forEach(objs, function(obj) {
+                    //add timestamp
+                    obj.timestamp = +new Date();
+                    obj.func.call(obj.thisArg || null, obj, data);
+                });
+            }
+        });
+
+        console.trace('emitting ' + evtArr.join());
+        return this;
+    };
+};
+
+module.exports = EventEmitter;
