33313f0390563ec8dd2a2a7e7af48a6112b450e8
yinyong <yinyong@sogou-inc.com>
2014-08-05 18:10:50 +0800

    remove wrapper of buggy

--- src/js/buggy.js
+++ src/js/buggy.js
@@ -11,33 +11,31 @@
  * @since 0.1.0
  */
 
-(function(window, document, undefined) {
-    "use strict";
+"use strict";
 
-    var expando = require('./type').expando;
+var expando = require('./type').expando;
 
-    var Buggy = {
-        /**
-         * "getElementById" is buggy on IE6/7.
-         *
-         * @see  https://github.com/jquery/sizzle/blob/96728dd43c62dd5e94452f18564a888e7115f936/src/sizzle.js#L528
-         * @property
-         */
-        getElementById: (function(document) {
-            var div = document.createElement('div');
+var Buggy = {
+    /**
+     * "getElementById" is buggy on IE6/7.
+     *
+     * @see  https://github.com/jquery/sizzle/blob/96728dd43c62dd5e94452f18564a888e7115f936/src/sizzle.js#L528
+     * @property
+     */
+    getElementById: (function(document) {
+        var div = document.createElement('div');
 
-            //document.body is null here
-            document.documentElement.appendChild(div).setAttribute('id', expando);
+        //document.body is null here
+        document.documentElement.appendChild(div).setAttribute('id', expando);
 
-            var buggy = document.getElementsByName && document.getElementsByName(expando).length;
+        var buggy = document.getElementsByName && document.getElementsByName(expando).length;
 
-            document.documentElement.removeChild(div);
+        document.documentElement.removeChild(div);
 
-            div = null;
+        div = null;
 
-            return !!buggy;
-        })(document)
-    };
+        return !!buggy;
+    })(document)
+};
 
-    module.exports = Buggy;
-})(window, document);
+module.exports = Buggy;
