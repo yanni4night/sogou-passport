600159ae9633f6aee2b94802053d7afa7a3acffc
yinyong <yinyong@sogou-inc.com>
2014-08-05 18:11:32 +0800

    remove wrapper of array

--- src/js/array.js
+++ src/js/array.js
@@ -13,158 +13,156 @@
  * @since 0.1.0
  */
 
-(function(undefined) {
-  "use strict";
+"use strict";
 
-  var type = require('./type');
-  var array = {};
+var type = require('./type');
+var array = {};
 
-  /**
-   * ES5 array indexOf polyfill.
-   *
-   * @param  {Array} arr
-   * @param  {Object} ele
-   * @param  {Integer} fromIndex
-   * @return {Boolean}
-   */
-  array.indexOf = function(arr, ele, fromIndex) {
-    var i, len;
+/**
+ * ES5 array indexOf polyfill.
+ *
+ * @param  {Array} arr
+ * @param  {Object} ele
+ * @param  {Integer} fromIndex
+ * @return {Boolean}
+ */
+array.indexOf = function(arr, ele, fromIndex) {
+  var i, len;
 
-    if (!arr || !ele) {
-      return -1;
-    }
+  if (!arr || !ele) {
+    return -1;
+  }
 
-    fromIndex = fromIndex | 0;
-    if (isNaN(fromIndex)) {
+  fromIndex = fromIndex | 0;
+  if (isNaN(fromIndex)) {
+    fromIndex = 0;
+  }
+  if (fromIndex >= arr.length) {
+    return -1;
+  }
+  if (fromIndex < 0) {
+    fromIndex = arr.length + fromIndex;
+    if (fromIndex < 0) {
       fromIndex = 0;
     }
-    if (fromIndex >= arr.length) {
-      return -1;
-    }
-    if (fromIndex < 0) {
-      fromIndex = arr.length + fromIndex;
-      if (fromIndex < 0) {
-        fromIndex = 0;
+  }
+
+  if (Array.prototype.indexOf) {
+    return Array.prototype.indexOf.call(arr, ele, fromIndex);
+  } else {
+    for (i = fromIndex, len = arr.length; i < len; ++i) {
+      if (ele === arr[i]) {
+        return i;
       }
     }
+  }
 
-    if (Array.prototype.indexOf) {
-      return Array.prototype.indexOf.call(arr, ele, fromIndex);
-    } else {
-      for (i = fromIndex, len = arr.length; i < len; ++i) {
-        if (ele === arr[i]) {
-          return i;
-        }
-      }
-    }
+  return -1;
+};
 
-    return -1;
-  };
+/**
+ * ES5 array forEach polyfill.
+ *
+ * @param  {Array} arr
+ * @param  {Function} callbackfn
+ * @param  {Object} thisArg
+ * @return {Undefined}
+ */
+array.forEach = function(arr, callbackfn, thisArg) {
+  var i, len;
 
-  /**
-   * ES5 array forEach polyfill.
-   *
-   * @param  {Array} arr
-   * @param  {Function} callbackfn
-   * @param  {Object} thisArg
-   * @return {Undefined}
-   */
-  array.forEach = function(arr, callbackfn, thisArg) {
-    var i, len;
+  type.assertNonNullOrUndefined('arr', arr);
+  type.assertFunction('callbackfn', callbackfn);
 
-    type.assertNonNullOrUndefined('arr', arr);
-    type.assertFunction('callbackfn', callbackfn);
+  if (Array.prototype.forEach) {
+    return Array.prototype.forEach.call(arr, callbackfn, thisArg);
+  }
 
-    if (Array.prototype.forEach) {
-      return Array.prototype.forEach.call(arr, callbackfn, thisArg);
-    }
+  for (i = 0, len = arr.length; i < len; ++i) {
+    callbackfn.call(thisArg, arr[i], i, arr);
+  }
 
+};
+/**
+ * ES5 array every polyfill.
+ *
+ * @param  {Array} arr
+ * @param  {Function} func
+ * @return {Boolean}
+ */
+array.each = array.every = function(arr, callbackfn, thisArg) {
+  var i, len;
+
+  type.assertNonNullOrUndefined('arr', arr);
+  type.assertFunction('callbackfn', callbackfn);
+
+  //ES5
+  if (Array.prototype.every) {
+    return Array.prototype.every.call(arr, callbackfn, thisArg);
+  } else {
     for (i = 0, len = arr.length; i < len; ++i) {
-      callbackfn.call(thisArg, arr[i], i, arr);
-    }
-
-  };
-  /**
-   * ES5 array every polyfill.
-   *
-   * @param  {Array} arr
-   * @param  {Function} func
-   * @return {Boolean}
-   */
-  array.each = array.every = function(arr, callbackfn, thisArg) {
-    var i, len;
-
-    type.assertNonNullOrUndefined('arr', arr);
-    type.assertFunction('callbackfn', callbackfn);
-
-    //ES5
-    if (Array.prototype.every) {
-      return Array.prototype.every.call(arr, callbackfn, thisArg);
-    } else {
-      for (i = 0, len = arr.length; i < len; ++i) {
-        if (!callbackfn.call(thisArg, arr[i], i, arr)) {
-          return false;
-        }
+      if (!callbackfn.call(thisArg, arr[i], i, arr)) {
+        return false;
       }
     }
+  }
 
-    return true;
-  };
+  return true;
+};
 
-  /**
-   * ES5 array some polyfill.
-   *
-   * @param  {Array} arr
-   * @param  {Function} callbackfn
-   * @param  {Object} thisArg
-   * @return {Boolean}
-   */
-  array.some = function(arr, callbackfn, thisArg) {
-    var i, len;
+/**
+ * ES5 array some polyfill.
+ *
+ * @param  {Array} arr
+ * @param  {Function} callbackfn
+ * @param  {Object} thisArg
+ * @return {Boolean}
+ */
+array.some = function(arr, callbackfn, thisArg) {
+  var i, len;
 
-    type.assertNonNullOrUndefined('arr', arr);
-    type.assertFunction('callbackfn', callbackfn);
+  type.assertNonNullOrUndefined('arr', arr);
+  type.assertFunction('callbackfn', callbackfn);
 
-    //ES5
-    if (Array.prototype.some) {
-      return Array.prototype.some.call(arr, callbackfn, thisArg);
-    } else {
-      for (i = 0, len = arr.length; i < len; ++i) {
-        if (true === callbackfn.call(thisArg, arr[i], i, arr)) {
-          return true;
-        }
+  //ES5
+  if (Array.prototype.some) {
+    return Array.prototype.some.call(arr, callbackfn, thisArg);
+  } else {
+    for (i = 0, len = arr.length; i < len; ++i) {
+      if (true === callbackfn.call(thisArg, arr[i], i, arr)) {
+        return true;
       }
     }
+  }
 
-    return false;
-  };
+  return false;
+};
 
-  /**
-   * ES5 array filter polyfill.
-   *
-   * @param  {Array} arr
-   * @param  {Function} callbackfn
-   * @param  {Object} thisArg
-   * @return {Array}
-   */
-  array.filter = function(arr, callbackfn, thisArg) {
-    var ret = [];
+/**
+ * ES5 array filter polyfill.
+ *
+ * @param  {Array} arr
+ * @param  {Function} callbackfn
+ * @param  {Object} thisArg
+ * @return {Array}
+ */
+array.filter = function(arr, callbackfn, thisArg) {
+  var ret = [];
 
-    type.assertNonNullOrUndefined('arr', arr);
-    type.assertFunction('callbackfn', callbackfn);
+  type.assertNonNullOrUndefined('arr', arr);
+  type.assertFunction('callbackfn', callbackfn);
 
 
-    if (Array.prototype.filter) {
-      return Array.prototype.filter.call(arr, callbackfn, thisArg);
+  if (Array.prototype.filter) {
+    return Array.prototype.filter.call(arr, callbackfn, thisArg);
+  }
+
+  array.forEach(arr, function(val, index) {
+    if (callbackfn.call(thisArg, val, index, arr)) {
+      ret.push(val);
     }
+  });
+  return ret;
+};
 
-    array.forEach(arr, function(val, index) {
-      if (callbackfn.call(thisArg, val, index, arr)) {
-        ret.push(val);
-      }
-    });
-    return ret;
-  };
-
-  module.exports = array;
-})();
+module.exports = array;
